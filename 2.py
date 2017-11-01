15d052317b33dea85b8a584dd7da4acb14fb1762
1617193124 <33270476+1617193124@users.noreply.github.com>
2017-10-31 20:34:09 -0500

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,14 @@
+num = 9
+if num >= 0 and num <= 10:
+    print 'hello'
+
+num = 10
+if num < 0 or num > 10:
+    print 'hello'
+else:
+    print 'undefine'
+
+if (num >= 0 and num <= 5) or (num >= 10 and num <= 15):
+    print 'hello'
+else:
+    print 'undefine'
