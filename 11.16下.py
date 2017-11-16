9d1b0457d2b16ff5016b6d388c945a531788a87d
1617193124 <33270476+1617193124@users.noreply.github.com>
2017-11-16 14:15:21 +0800

    Add files via upload

---
+++ 11.16下.py
@@ -0,0 +1,33 @@
+Python 2.7.13 (v2.7.13:a06454b1afa1, Dec 17 2016, 20:42:59) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> import urllib2
+>>> httpHandler = urllib2.HTTPHandler(debuglevel=1)
+>>> httpsHandler = urllib2.HTTPSHandler(debuglevel=1)
+>>> opener = urllib2.build_opener(httpHandler, httpsHandler)
+>>> urllib2.install_opener(opener)
+>>> 
+response = urllib2.urlopen('http://www.baidu.com')
+send: 'GET / HTTP/1.1\r\nAccept-Encoding: identity\r\nHost: www.baidu.com\r\nConnection: close\r\nUser-Agent: Python-urllib/2.7\r\n\r\n'
+reply: 'HTTP/1.1 200 OK\r\n'
+header: Date: Thu, 16 Nov 2017 06:14:32 GMT
+header: Content-Type: text/html; charset=utf-8
+header: Transfer-Encoding: chunked
+header: Connection: Close
+header: Vary: Accept-Encoding
+header: Set-Cookie: BAIDUID=12BDA06BDCF3E6D2C679DD84EE2DC345:FG=1; expires=Thu, 31-Dec-37 23:55:55 GMT; max-age=2147483647; path=/; domain=.baidu.com
+header: Set-Cookie: BIDUPSID=12BDA06BDCF3E6D2C679DD84EE2DC345; expires=Thu, 31-Dec-37 23:55:55 GMT; max-age=2147483647; path=/; domain=.baidu.com
+header: Set-Cookie: PSTM=1510812872; expires=Thu, 31-Dec-37 23:55:55 GMT; max-age=2147483647; path=/; domain=.baidu.com
+header: Set-Cookie: BDSVRTM=0; path=/
+header: Set-Cookie: BD_HOME=0; path=/
+header: Set-Cookie: H_PS_PSSID=1461_24569_21116_17001_22159; path=/; domain=.baidu.com
+header: P3P: CP=" OTI DSP COR IVA OUR IND COM "
+header: Cache-Control: private
+header: Cxy_all: baidu+70154e4c79b554a6f67e06426a7cb36f
+header: Expires: Thu, 16 Nov 2017 06:13:36 GMT
+header: X-Powered-By: HPHP
+header: Server: BWS/1.1
+header: X-UA-Compatible: IE=Edge,chrome=1
+header: BDPAGETYPE: 1
+header: BDQID: 0x8f3f5d4400003a67
+header: BDUSERID: 0
+>>> 
